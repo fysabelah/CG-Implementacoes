28aea8e79b1388a9a47f10076faadec4ec790b6a
Isabela França <fysabelah@gmail.com>
2020-09-30 18:25:13 -0300

    Diferente forma de implementação das funções de rotação, translação e escala. (#1)

---
+++ Entrega 1/trabalho1.c
@@ -0,0 +1,108 @@
+#include <GL/glut.h> 
+#include <stdlib.h> 
+#include<math.h>
+#define tx 0.5
+#define ty 0.5
+#define ang 30
+//x1 = 0.25 y1 = 0.25, x2 = 0.75,y2 = 0.25,x3 = 0.75, y3 = 0.95, x4 = 0.25, y4 = 0.95;
+
+GLfloat pontos[3]= {0.15, 0.35, 0.55};
+
+void normal(void);
+void translacao(void);
+void escala(void);
+void rotacao(void);
+
+int main(int argc, char** argv){ 
+	//Normal
+	glutInit(&argc, argv); 
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize (300, 300); 
+	glutInitWindowPosition (100, 100); 
+	glutCreateWindow ("Transformações"); 
+	glClearColor(1.0, 1.0, 1.0, 0.0); 
+	glShadeModel (GL_FLAT); 
+	glOrtho (0, 1, 0, 1, -1 ,1); 
+	glutDisplayFunc(normal); 
+	
+	//Translação
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize (300, 300); 
+	glutInitWindowPosition (250, 250); 
+	glutCreateWindow ("Translação");
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glutDisplayFunc(translacao);
+	
+	//Escala
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize (300, 300); 
+	glutInitWindowPosition (350, 350); 
+	glutCreateWindow ("Escala");
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glutDisplayFunc(escala);
+
+	
+	//Rotação
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize (300, 300); 
+	glutInitWindowPosition (450, 450); 
+	glutCreateWindow ("ROtação");
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glutDisplayFunc(escala);
+	
+
+	glutMainLoop();	
+	
+	return 0; 
+} 
+
+void rotacao(void){
+	glClear(GL_COLOR_BUFFER_BIT); 
+	glColor3f (0.0, 0.0, 0.0); 
+	glBegin(GL_POLYGON); 
+	glVertex2f(pontos[0] * cos(ang) - pontos[0] * sin(ang), pontos[0] * sin(ang) + pontos[0] * cos(ang)); 
+	glVertex2f(pontos[1] * cos(ang) - pontos[0] * sin(ang), pontos[1] * sin(ang) + pontos[0] * cos(ang)); 
+	glVertex2f(pontos[1] * cos(ang) - pontos[2] * sin(ang), pontos[1] * sin(ang) + pontos[2] * cos(ang)); 
+	glVertex2f(pontos[0] * cos(ang) - pontos[2] * sin(ang), pontos[0] * sin(ang) + pontos[2] * cos(ang)); 
+	glEnd(); 
+	glFlush();	
+	
+}
+
+void escala(void){
+	glClear(GL_COLOR_BUFFER_BIT); 
+	glColor3f (0.0, 0.0, 0.0); 
+	glBegin(GL_POLYGON); 
+	glVertex2f(pontos[0] * tx, pontos[0] * ty); 
+	glVertex2f(pontos[1] * tx, pontos[0] * ty); 
+	glVertex2f(pontos[1] * tx, pontos[2] * ty); 
+	glVertex2f(pontos[0] * tx, pontos[2] * ty); 
+	glEnd(); 
+	glFlush();	
+	
+}
+
+void translacao(void){
+	glClear(GL_COLOR_BUFFER_BIT); 
+	glColor3f (0.0, 0.0, 0.0); 
+	glBegin(GL_POLYGON); 
+	glVertex2f(pontos[0] + tx, pontos[0] + ty); 
+	glVertex2f(pontos[1] + tx, pontos[0] + ty); 
+	glVertex2f(pontos[1] + tx, pontos[2] + ty); 
+	glVertex2f(pontos[0] + tx, pontos[2] + ty); 
+	glEnd(); 
+	glFlush();		
+}
+
+void normal(void){
+	glClear(GL_COLOR_BUFFER_BIT); 
+	glColor3f (0.0, 0.0, 0.0); 
+	glBegin(GL_POLYGON); 
+	glVertex2f(pontos[0], pontos[0]); 
+	glVertex2f(pontos[1], pontos[0]); 
+	glVertex2f(pontos[1], pontos[2]); 
+	glVertex2f(pontos[0], pontos[2]); 
+	glEnd(); 
+	glFlush();	
+	
+}
--- Entrega 1/trabalho1.cpp
+++ Entrega 1/trabalho1.cpp
@@ -3,70 +3,67 @@
 #include<math.h>
 
 void triangulo(float ax, float ay, float bx, float by,GLdouble theta,float cx, float cy, float cz){
+	GLfloat coss,seno,normal; //declaracao de tipos necessarios para rotacao
 
-GLfloat coss,seno,normal; //declaracao de tipos necessarios para rotacao
+	if (theta > 0){
+		coss = cos(theta); //cosseno de theta para a formula da rotacao
+		seno = sin(theta); //seno de theta para a rotacao
+	}
+	else {
+		coss = 0; //Para o caso de nao haver rotacao
+		seno = 0;	
+	}	
 
-if (theta > 0){
-	coss = cos(theta); //cosseno de theta para a formula da rotacao
-	seno = sin(theta); //seno de theta para a rotacao
+	normal = sqrt(pow(cx,cx)+pow(cy,cy)+pow(cz,cz)); //normalizacao das variaveis, senao aplicaria mais de uma funcao
+
+	if (normal < 1.0e-4) { //Padrao que vi na implementacao padrao
+		cx /= normal; //normalizando x
+		cz /= normal; //normalizando y
+		cy /= normal; //normalizando z
+	}
+
+	glBegin(GL_TRIANGLES);
+
+	glColor3f(0,1,0);  //abaixo aplicam-se todas as operacoes, translacao, escala e rotacao, respectivamente 
+	glVertex3f((((-2+ax)*bx)+(pow(cx,cx)*(1-coss) + coss)),((2+ay)*by+((cx*cy)*(1-coss) - (cz * seno))),(0 + ((cx*cz)*(1-coss) + (cy * seno))));
+
+	glColor3f(0,0,1);
+	glVertex3f((((-1+ax)*bx)+((cy*cx)*(1-coss) + (cz * seno))),(((3+ay)*by)+(pow(cy,cy)*(1-coss) + coss)),(0+((cy*cz)*(1-coss) - (cx * seno))));
+
+	glColor3f(1,0,1);
+	glVertex3f(((0+ax)*bx+((cx*cz)*(1-coss) - (cy * seno))),((2+ay)*by+((cy*cz)*(1-coss) + (cx * seno))),0+(pow(cz,cz)*(1-coss) + coss));
 }
-else {
-	coss = 0; //Para o caso de nao haver rotacao
-	seno = 0;	
-}	
-
-normal = sqrt(pow(cx,cx)+pow(cy,cy)+pow(cz,cz)); //normalizacao das variaveis, senao aplicaria mais de uma funcao
-
-if (normal < 1.0e-4) { //Padrao que vi na implementacao padrao
-	cx /= normal; //normalizando x
-	cz /= normal; //normalizando y
-	cy /= normal; //normalizando z
-}
-
-glBegin(GL_TRIANGLES);
-
-glColor3f(0,1,0);  //abaixo aplicam-se todas as operacoes, translacao, escala e rotacao, respectivamente 
-glVertex3f((((-2+ax)*bx)+(pow(cx,cx)*(1-coss) + coss)),((2+ay)*by+((cx*cy)*(1-coss) - (cz * seno))),(0 + ((cx*cz)*(1-coss) + (cy * seno))));
-
-glColor3f(0,0,1);
-glVertex3f((((-1+ax)*bx)+((cy*cx)*(1-coss) + (cz * seno))),(((3+ay)*by)+(pow(cy,cy)*(1-coss) + coss)),(0+((cy*cz)*(1-coss) - (cx * seno))));
-
-glColor3f(1,0,1);
-glVertex3f(((0+ax)*bx+((cx*cz)*(1-coss) - (cy * seno))),((2+ay)*by+((cy*cz)*(1-coss) + (cx * seno))),0+(pow(cz,cz)*(1-coss) + coss));
-}
-
 
 void init(){
-glClearColor(0,0,0,0);
-gluOrtho2D(-5,5,-5,5);
+	glClearColor(0,0,0,0);
+	gluOrtho2D(-5,5,-5,5);
 }
 
-
 void display(){
-GLdouble theta = 90; //declaracao de theta
-glClear(GL_COLOR_BUFFER_BIT);
-glPushMatrix();
+	GLdouble theta = 90; //declaracao de theta
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
 
-triangulo(0,0,1,1,0,0,0,0); 
-//ax,ay sao parametros da translocacao
-//bx, by sao parametros da escala
-//theta, cx,cy,cz sao parametros da rotacao
-//a funcao recebe na respectiva ordem acima
+	triangulo(0,0,1,1,0,0,0,0); 
+	//ax,ay sao parametros da translocacao
+	//bx, by sao parametros da escala
+	//theta, cx,cy,cz sao parametros da rotacao
+	//a funcao recebe na respectiva ordem acima
 
-glEnd();
+	glEnd();
 
-glPopMatrix();
-glFlush();
+	glPopMatrix();
+	glFlush();
 }
 
 int main(int argc, char ** argv){
-glutInit(&argc, argv);
-glutInitWindowSize(600,400);
-glutInitWindowPosition(10,50);
-glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);    
-glutCreateWindow("Transformacoes");
-init();
-glutDisplayFunc(display);
-glutMainLoop();
+	glutInit(&argc, argv);
+	glutInitWindowSize(600,400);
+	glutInitWindowPosition(10,50);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);    
+	glutCreateWindow("Transformacoes");
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
 }
 
